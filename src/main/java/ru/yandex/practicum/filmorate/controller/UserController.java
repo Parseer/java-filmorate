cedbe1fc15051b26502b49c20f946b016f1e3fc4
unknown <alexanderevgendolgov@yandex.ru>
2026-01-05 21:48:03 +0500

    POMOGITEE

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -50,11 +50,9 @@ public class UserController {
 
         try {
             validateUser(user);
-
             if (user.getName() == null || user.getName().trim().isEmpty()) {
                 user.setName(user.getLogin());
             }
-
             user.setId(nextId++);
             users.put(user.getId(), user);
 
